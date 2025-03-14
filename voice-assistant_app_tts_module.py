f3085f8cae7db696d4b435ee613453d8d4c4042a
Ashton94949 <97370406+Ashton94949@users.noreply.github.com>
2025-03-14 08:08:32 -0500

    Update voice-assistant_app_tts_module.py

--- voice-assistant_app_tts_module.py
+++ voice-assistant_app_tts_module.py
@@ -4,7 +4,5 @@ import os
 def text_to_speech(text):
     tts = gTTS(text=text, lang='en')
     tts.save("response.mp3")
-    with open("response.mp3", "rb") as audio_file:
-        audio_data = audio_file.read()
+    os.system("mpg321 response.mp3")
     os.remove("response.mp3")
-    return audio_data
